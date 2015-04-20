e0053a33a60865a1710606feb61a7708d8144336
gdwangh <gdwangh@189.cn>
2015-04-20 23:20:58 +0800

    update competition: combine 2 model

--- kaggleCompetition/study9.R
+++ kaggleCompetition/study9.R
@@ -1,5 +1,5 @@
-# setwd("D:/doc/study/TheAnalyticsEdge/kaggleCompetition")
-setwd("D:/workspace/The Analytics Edge/kaggleCompetition")
+setwd("D:/doc/study/TheAnalyticsEdge/kaggleCompetition")
+#setwd("D:/workspace/The Analytics Edge/kaggleCompetition")
 
 NewsTrain = read.csv("NYTimesBlogTrain.csv", stringsAsFactors=FALSE)
 NewsTest = read.csv("NYTimesBlogTest.csv", stringsAsFactors=FALSE)
@@ -26,8 +26,8 @@ SubsectionNameFactor = as.factor(c(NewsTrain$SubsectionName, NewsTest$Subsection
 NewsTrain$SubsectionName = SubsectionNameFactor[1:(nrow(NewsTrain))]
 NewsTest$SubsectionName = SubsectionNameFactor[(nrow(NewsTrain)+1):length(SubsectionNameFactor)]
 
-NewsTrain$logWordCount = log(NewsTrain$WordCount+1)
-NewsTest$logWordCount = log(NewsTest$WordCount+1)
+# NewsTrain$logWordCount = log(NewsTrain$WordCount+1)
+# NewsTest$logWordCount = log(NewsTest$WordCount+1)
 
 # split train data to training set and valid set
 library(caTools)
@@ -36,13 +36,22 @@ spl = sample.split(NewsTrain$Popular, 0.7)
 Train = subset(NewsTrain, spl==TRUE)
 Valid = subset(NewsTrain, spl==FALSE)
 
+# glm
+set.seed(12345)
+SimpleLog = glm(Popular ~ WordCount+NewsDesk+Hour+Weekday+SectionName+SubsectionName, data=Train, family="binomial")
+SimpleLog.Pred = predict(SimpleLog, newdata=Valid, type="response")
+                
 # randomForest 
 library(randomForest)
 set.seed(12345)
 SimpleRF = randomForest(Popular ~ WordCount+NewsDesk+Hour+Weekday+SectionName+SubsectionName, data=Train)
-Pred = predict(SimpleRF, newdata=Valid, type="prob")
-table(Valid$Popular, Pred[,2]>0.5)  
+SimpleRF.Pred = predict(SimpleRF, newdata=Valid, type="prob")
+
+pred = (SimpleLog.Pred + SimpleRF.Pred[,2])/2
+table(Valid$Popular, pred>0.5)  
 (1571+220)/nrow(Valid)  # 0.9137755
+
+(1563+224)/nrow(Valid)  # 0.9117347
 
 
 # auc
@@ -52,53 +61,11 @@ ROCR.SimpleLog.pred = prediction(SimpleRF.pred[,2], Train$Popular)
 auc = as.numeric(performance(ROCR.SimpleLog.pred, "auc")@y.values)
 auc  # 0.9366087
 
-library(caret)
-fitControl <- trainControl(method = "repeatedcv",
-                           number = 10,
-                           repeats = 10,
-                           ## Estimate class probabilities
-                           classProbs = TRUE,
-                           ## Evaluate performance using 
-                           ## the following function
-                           summaryFunction = twoClassSummary)
 
-gbmGrid <-  expand.grid(interaction.depth = c(1, 5, 9),
-                        n.trees = (1:30)*50,
-                        shrinkage = 0.1)
+# test
+SimpleLog.Pred = predict(SimpleLog, newdata=NewsTest, type="response")
+SimpleRF.Pred = predict(SimpleRF, newdata=NewsTest, type="prob")
+PredTest = (SimpleLog.Pred + SimpleRF.Pred[,2])/2
 
-set.seed(825)
-gbmFit3 <- train(Popular ~ WordCount+NewsDesk+Hour+Weekday+SectionName+SubsectionName, 
-                 data = Train,
-                 method = "gbm",
-                 trControl = fitControl,
-                 verbose = FALSE,
-                 tuneGrid = gbmGrid,
-                 ## Specify which metric to optimize
-                 metric = "ROC")
-
-pred = predict(gbmFit3, newdata=Valid, type = "prob")
-table(pred, Valid$Popular)
-(1571+212)/nrow(Valid)
-
-# gbm
-# library(gbm)
-# # gbmFit <- gbm(Popular ~ WordCount+NewsDesk+Hour+Weekday+SectionName+SubsectionName, data=Train,shrinkage=0.01, distribution='bernoulli',cv.folds=5,n.trees=3000,verbose=F)
-# gbmFit <- gbm(Popular ~ WordCount+NewsDesk+Hour+Weekday+SectionName+SubsectionName, data=Train,
-#               distribution = "bernoulli",n.trees=1000,cv.folds = 3)
-# best.iter <- gbm.perf(gbmFit,method="test")
-# 
-# # 用交叉检验确定最佳迭代次数
-# best.iter <- gbm.perf(gbmFit,method='cv')
-# 
-# Pred = predict(gbmFit, newdata=Valid, n.trees=100)
-# table(Valid$Popular, Pred[,2]>0.5)  
-
-
-# xgboost: eXtreme Gradient Boosting
-# install.packages('xgboost')
-# devtools::install_github('dmlc/xgboost',subdir='R-package')
-# library(xgboost)
-# library(methods)
-# label <- Train$Popular
-# data <- as.matrix(Train[,1:3])
-# xgmat <- xgb.DMatrix(data, label = label)
+MySubmission = data.frame(UniqueID = NewsTest$UniqueID, Probability1 = PredTest)
+write.csv(MySubmission, "SubmissionLogRF.csv", row.names=FALSE)
