d8da692093124b7bf706b7e9616763fba2a5fa9c
gdwangh <gdwangh@189.cn>
2015-05-06 22:13:03 +0800

    update notes for files shared by others

--- kaggleCompetition/otherShared/featureEngineer.R
+++ kaggleCompetition/otherShared/featureEngineer.R
@@ -1,15 +1,18 @@
-setwd("D:/workspace/The Analytics Edge/kaggleCompetition")
+# setwd("D:/workspace/The Analytics Edge/kaggleCompetition")
+setwd("D:/doc/study/TheAnalyticsEdge/kaggleCompetition")
 
 newsTrain <- read.csv("NYTimesBlogTrain.csv", stringsAsFactors=FALSE)
 newsTest <- read.csv("NYTimesBlogTest.csv", stringsAsFactors=FALSE)
 
-all <- rbind(newsTrain[,!(colnames(newsTrain) %in% "Popular")], newsTest)
-
-# CorpusHeadline = Corpus(VectorSource(c(all$Headline)))
-Popu = subset(newsTrain, Popular==1)
-
 library(tm)
-CorpusHeadline = Corpus(VectorSource(c(Popu$Headline)))
+
+# all <- rbind(newsTrain[,!(colnames(newsTrain) %in% "Popular")], newsTest)
+# CorpusHeadline = Corpus(VectorSource(c(all$Headline)))
+
+# Popu = subset(newsTrain, Popular==1)
+# CorpusHeadline = Corpus(VectorSource(c(Popu$Headline)))
+# CorpusHeadline = Corpus(VectorSource(c(newsTrain$Headline)))
+CorpusHeadline = Corpus(VectorSource(c(newsTest$Headline)))
 
 # You can go through all of the standard pre-processing steps like we did in Unit 5:
 CorpusHeadline = tm_map(CorpusHeadline, tolower)
@@ -23,17 +26,19 @@ CorpusHeadline = tm_map(CorpusHeadline, stemDocument)
 require(RWeka)
 library(rJava)
 TrigramTokenizer <- function(x) RWeka::NGramTokenizer(x, RWeka::Weka_control(min = 1, max = 3))  
-dtm <- DocumentTermMatrix(CorpusHeadline, control = list(tokenize = TrigramTokenizer))
+dtm <- DocumentTermMatrix(CorpusHeadline, control = list(tokenize = TrigramTokenizer, weighting=function(x) weightTfIdf(x, normalize=FALSE)))
 
+sparse = removeSparseTerms(dtm, 0.999)
 
-HeadlineWords = as.data.frame(as.matrix(dtm))
+# HeadlineWords = as.data.frame(as.matrix(dtm))
+HeadlineWords = as.data.frame(as.matrix(sparse))
 
 library(wordcloud)
 wordcloud(colnames(HeadlineWords), colSums(HeadlineWords),colors=brewer.pal(9, "Blues")[c(-1,-2,-3,-4)])
 
 sort(colSums(HeadlineWords))
 
-flag <- as.factor(ifelse(grepl("recap", newsTrain$Headline, ignore.case=TRUE)==TRUE, "Yes","No"))
+flag <- as.factor(ifelse(grepl("week", newsTrain$Headline, ignore.case=TRUE)==TRUE, "Yes","No"))
 table(newsTrain$Popular, flag)
 prop.table(table(newsTrain$Popular, flag),2)
 tapply(newsTrain$Popular,flag,mean)
