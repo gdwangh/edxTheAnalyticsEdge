d93835bbff8d91c11fd509f400f1582ca4d73ad3
gdwangh <gdwangh@189.cn>
2015-06-02 21:51:18 +0800

    complete competition study after competition end

--- kaggleCompetition/post-study/final.R
+++ kaggleCompetition/post-study/final.R
@@ -29,8 +29,8 @@ newsTest$Hour = newsTest$PubDate$hour
 mon = newsTrain$PubDate$mon
 dd = newsTrain$PubDate$mday
 newsTrain$isHoliday = (mon == 8 & dd == 1) | (mon == 8 & dd == 11) | (mon==9 & dd==13 ) | 
-                      (mon==9 & dd == 31) | (mon == 10 & dd == 1) | (mon==10 & dd==11) |
-                      (mon==10 & dd == 27)
+  (mon==9 & dd == 31) | (mon == 10 & dd == 1) | (mon==10 & dd==11) |
+  (mon==10 & dd == 27)
 
 # newsTest: mon=11: 12月
 # 12月24日 平安夜
@@ -145,7 +145,7 @@ tmp = merge(newsTrain[,c("UniqueID","yday")], newsTrain_state[,c(1,2)], by="yday
 newsTrain$everydayCount = tmp$count
 
 newsTest_state<- tbl_df(newsTest) %>% select(yday) %>% group_by(yday) %>% 
-            summarise(count = n())
+  summarise(count = n())
 tmp = merge(newsTest[,c("UniqueID","yday")], newsTest_state, by="yday")
 newsTest$everydayCount = tmp$count
 
@@ -174,14 +174,14 @@ newsAll$last50 = as.numeric(merge(z-lag(z, -50), b, all = TRUE))
 
 # cal the average of last1/3/5/10/20/50
 last.avg = newsAll[, c("Weekday", "Hour", "last1", "last3", "last5", "last10", "last20", "last50")] %>% 
-          group_by(Weekday, Hour) %>% dplyr::summarise(
-  last1.avg=mean(last1, na.rm=TRUE),
-  last3.avg=mean(last3, na.rm=TRUE),
-  last5.avg=mean(last5, na.rm=TRUE),
-  last10.avg=mean(last10, na.rm=TRUE),
-  last20.avg=mean(last20, na.rm=TRUE),
-  last50.avg=mean(last50, na.rm=TRUE)
-)
+  group_by(Weekday, Hour) %>% dplyr::summarise(
+    last1.avg=mean(last1, na.rm=TRUE),
+    last3.avg=mean(last3, na.rm=TRUE),
+    last5.avg=mean(last5, na.rm=TRUE),
+    last10.avg=mean(last10, na.rm=TRUE),
+    last20.avg=mean(last20, na.rm=TRUE),
+    last50.avg=mean(last50, na.rm=TRUE)
+  )
 
 # replace na with average 
 na.merge = merge(newsAll, last.avg, by=c("Weekday","Hour"))
@@ -200,14 +200,7 @@ cor(newsTrain[,c("Popular","last1", "last3", "last5", "last10", "last20", "last5
 
 rm(last.avg, na.merge, b, i, idx, n, pd, y, z)
 
-boxplot(newsTrain$Popular, newsTrain$last10)
-
-glmFit = glm(PopularFactor~NewsDeskFactor+SectionNameFactor+SubsectionNameFactor+
-                          logWordCount+Weekday+Hour+headlineIsPopWord+
-                          AbstractPolarity+HSpolarity+
-                          last20,
-             data = newsTrain,  family=binomial)
-summary(glmFit)
+## ENSEMBLE
 
 # rf
 library(caret)
@@ -223,31 +216,219 @@ library(doParallel)
 cl<- makeCluster(detectCores()-1)  
 registerDoParallel(cl)
 
-rfGrid<- expand.grid(mtry=c(1:20))
-
-set.seed(1000)
-rfFit = train(PopularFactor~NewsDeskFactor+SectionNameFactor+SubsectionNameFactor+
-                            logWordCount+Weekday+Hour+headlineIsPopWord+
-                            AbstractPolarity+HSpolarity+everydayCount+isHoliday+
-                            last1+last3+last5+last10+last20+last50,
-              data = newsTrain,
-              method="rf",
-              importance=TRUE,
-              trControl=ensCtrl,
-              tuneGrid=rfGrid,
-              metric="ROC")
-
-stopCluster(cl)
+# rfGrid<- expand.grid(mtry=c(1:20))
+# 
+# set.seed(1000)
+# rfFit = train(PopularFactor~NewsDeskFactor+SectionNameFactor+SubsectionNameFactor+
+#                 logWordCount+Weekday+Hour+headlineIsPopWord+
+#                 AbstractPolarity+HSpolarity+everydayCount+isHoliday+
+#                 last1+last3+last5+last10+last20+last50,
+#               data = newsTrain,
+#               method="rf",
+#               importance=TRUE,
+#               trControl=ensCtrl,
+#               tuneGrid=rfGrid,
+#               metric="ROC")
 
 rfGrid<- expand.grid(mtry=c(9))
 
-pred.rf <- predict(rfFit, newdata=newsTrain, type='prob')
+stopCluster(cl)
+
+# # glmnet
+cl<- makeCluster(detectCores()-1)  
+registerDoParallel(cl)
+
+library(glmnet)
+x = newsTrain[,c("NewsDeskFactor","SectionNameFactor","SubsectionNameFactor","logWordCount",
+                  "Weekday","Hour","headlineIsPopWord","AbstractPolarity","HSpolarity",
+                  "everydayCount","isHoliday","last1","last3","last5","last10","last20","last50")]
+# set.seed(1000)
+# 
+# gla1 <- cv.glmnet(data.matrix(x), y=newsTrain$PopularFactor, nfolds = 10, alpha=1, type.measure="auc", family="binomial")
+# gla1$lambda.1se   # 0.02580755
+# gla1$lambda.min   # 0.00119788
+# 
+# pred1 = predict(gla1$glmnet.fit, newx=data.matrix(x), s = gla1$lambda.min)
+# 
+# gla0 <- cv.glmnet(data.matrix(x), y=newsTrain$PopularFactor, nfolds = 10, alpha=0, type.measure="auc", family="binomial")
+# gla0$lambda.1se  # 0.1170341
+# gla0$lambda.min  # 0.01041855
+# pred0 = predict(gla0$glmnet.fit, newx=data.matrix(x), s = gla0$lambda.min)
+# 
+# library("ROCR")
+# ROCR.Pred1 = prediction(pred1, newsTrain$PopularFactor)
+# auc1 = as.numeric(performance(ROCR.Pred1, "auc")@y.values)
+# auc1  # 0.8392424
+# 
+# ROCR.Pred0 = prediction(pred0, newsTrain$PopularFactor)
+# auc0 = as.numeric(performance(ROCR.Pred0, "auc")@y.values)
+# auc0  # 0.8381913
+# 
+# 0.0008256524
+# glmGrid<- expand.grid(alpha=seq(0, 1, by=0.1), lambda=c(1:100)*0.001)  # 0.001~0.1
+# 
+# set.seed(1000)
+# glmnetFit = train(PopularFactor~NewsDeskFactor+SectionNameFactor+SubsectionNameFactor+
+#                                     logWordCount+Weekday+Hour+headlineIsPopWord+
+#                                     AbstractPolarity+HSpolarity+everydayCount+isHoliday+
+#                                     last1+last3+last5+last10+last20+last50,
+#                   data = newsTrain,
+#                   method="glmnet", 
+#                   trControl=ensCtrl,
+#                   tuneGrid=glmGrid,
+#                   metric="ROC")
+
+# alpha=seq(0, 1, by=0.1), lambda=c(1:100)*0.001)=0.001~0.1: alpha = 1 and lambda = 0.003
+
+glmGrid<- expand.grid(alpha=c(1), lambda=0.003)
+
+stopCluster(cl)
+
+
+# gbm
+modelLookup("gbm")
+
+library(doParallel)
+cl<- makeCluster(detectCores()-1)  
+registerDoParallel(cl)
+
+# gbmGrid<- expand.grid(n.trees=c(10:30)*100, interaction.depth=c(10:20), shrinkage=c(0.005),n.minobsinnode = c(10))
+
+# ptm <- proc.time();ptm
+# 
+# set.seed(1000)
+# gbmFit = train(PopularFactor~NewsDeskFactor+SectionNameFactor+SubsectionNameFactor+
+#                              logWordCount+Weekday+Hour+headlineIsPopWord+
+#                              AbstractPolarity+HSpolarity+everydayCount+isHoliday+
+#                              last1+last3+last5+last10+last20+last50,
+#                data = newsTrain,
+#                method="gbm", 
+#                trControl=ensCtrl,
+#                tuneGrid=gbmGrid,
+#                metric="ROC")
+
+proc.time()-ptm
+用户     系统     流逝 
+366.17    14.36 69955.69 --- 19hours
+
+用户     系统     流逝 
+483.44    16.96 90940.41 --- 25.5
+
+用户     系统     流逝 
+222.17     1.67 40409.19 --- 11.2hour
+
+
+gbmGrid<- expand.grid(n.trees=c(1800), interaction.depth=c(19), shrinkage=c(.005),n.minobsinnode = 10)
+
+stopCluster(cl)
+
+
+pred.gbm = predict(gbmFit, type="prob")
+library("ROCR")
+ROCR.Pred.gbm = prediction( newsTrain$Popular, pred.gbm[,2]>0.5)
+auc1 = as.numeric(performance(ROCR.Pred.gbm, "auc")@y.values)
+auc1  # 0.9163987
+
+
+# svm
+modelLookup("svmRadial")
+
+library(e1071)
+library(doParallel)
+cl<- makeCluster(detectCores()-1)  
+registerDoParallel(cl)
+
+# svmGrid<- expand.grid(.sigma=c(.0007),.C=c(16,32))
+svmGrid<- expand.grid(.sigma=c(1:10)*.0001,.C=c(1:100)*0.1) #  sigma = 2e-04 and C = 9.2
+
+set.seed(1000)
+svmFit = train(PopularFactor~NewsDeskFactor+SectionNameFactor+SubsectionNameFactor+
+                             logWordCount+Weekday+Hour+headlineIsPopWord+
+                             AbstractPolarity+HSpolarity+everydayCount+isHoliday+
+                             last1+last3+last5+last10+last20+last50,
+               data = newsTrain,
+                method="svmRadial", 
+                trControl=ensCtrl,
+                tuneGrid=svmGrid,
+                 metric="ROC")
+
+svmGrid<- expand.grid(.sigma=c(0.0002),.C=c(9.2))
+
+library(caretEnsemble)
+set.seed(1000)
+model_list<- caretList(
+  PopularFactor~NewsDeskFactor+SectionNameFactor+SubsectionNameFactor+
+    logWordCount+Weekday+Hour+headlineIsPopWord+
+    AbstractPolarity+HSpolarity+everydayCount+isHoliday+
+    last1+last3+last5+last10+last20+last50,
+  data=newsTrain,
+  trControl=ensCtrl,
+  metric="ROC",
+  tuneList=list(
+    rf=caretModelSpec(method="rf", tuneGrid=rfGrid, nodesize=1, ntree=3000),
+    glmnet=caretModelSpec(method="glmnet",tuneGrid=glmGrid, preProcess=c("center","scale")),
+    gbm=caretModelSpec(method="gbm", tuneGrid=gbmGrid),
+    svm=caretModelSpec(method="svmRadial",tuneGrid=svmGrid, preProcess=c("center","scale"))
+  )
+)
+
+stopCluster(cl)
+
+greedy_ensemble<- caretEnsemble(model_list)
+
+library('caTools')
+model_preds<- lapply(model_list, predict, newdata=newsTrain, type='prob')
+model_preds<- lapply(model_preds, function(x) x[,'Yes'])
+model_preds<- data.frame(model_preds)
+
+ens_preds<- predict(greedy_ensemble, newdata=newsTrain)
+model_preds$ensemble<- ens_preds
+
+colAUC(model_preds, newsTrain$PopularFactor)
+
 
 library("ROCR")
-ROCR.Pred2 = prediction( newsTrain$Popular, pred.rf[,2]>0.5)
-auc = as.numeric(performance(ROCR.Pred2, "auc")@y.values)
-auc  # 0.9930345, 0.9917997,0.9950181
+ROCR.Pred = prediction( newsTrain$Popular, ens_preds>0.5)
+auc = as.numeric(performance(ROCR.Pred, "auc")@y.values)
+auc  # 0.918668
 
-pred.test = predict(rfFit, newdata=newsTest, type="prob")
-MySubmission = data.frame(UniqueID = newsTest$UniqueID, Probability1 = pred.test[,2])
-write.csv(MySubmission, "post-study/zoo.csv", row.names=FALSE)
+pred.test = predict(greedy_ensemble, newdata=newsTest)
+MySubmission = data.frame(UniqueID = newsTest$UniqueID, Probability1 = pred.test)
+write.csv(MySubmission, "post-study/caretEnsemble.csv", row.names=FALSE)
+
+# other method
+cl<- makeCluster(detectCores()-1)  
+registerDoParallel(cl)
+
+gbm_stack<- caretStack(
+  model_list,
+  method='gbm',
+  verbose=FALSE,
+  # tuneGrid=expand.grid(n.trees=c(2500), interaction.depth=c(24), shrinkage=c(.001),n.minobsinnode=c(10)),
+  metric='ROC',
+  trControl=trainControl(
+    method='cv',
+    number=10,
+    savePredictions=TRUE,
+    classProbs=TRUE,
+    allowParallel=TRUE,
+    summaryFunction=twoClassSummary
+  )
+)
+
+
+stopCluster(cl)
+
+model_preds2 <- model_preds
+model_preds2$ensemble <- predict(gbm_stack, newdata=newsTrain, type='prob')$Yes
+colAUC(model_preds2, newsTrain$PopularFactor)
+
+library("ROCR")
+ROCR.Pred2 = prediction( newsTrain$Popular, model_preds2$ensemble>0.5)
+auc = as.numeric(performance(ROCR.Pred2, "auc")@y.values)
+auc  # 0.964333
+
+
+pred.test = predict(gbm_stack, newdata=newsTest, type='prob')$Yes
+MySubmission = data.frame(UniqueID = newsTest$UniqueID, Probability1 = pred.test)
+write.csv(MySubmission, "post-study/ensembleModel_gbm.csv", row.names=FALSE)
